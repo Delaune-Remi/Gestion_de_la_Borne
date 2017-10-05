2a1353ee8ce419cac495fc3f8c2121eda6cb0ec4
Rémi Delaune <32340481+Delaune-Remi@users.noreply.github.com>
2017-10-05 11:21:00 +0200

    Delete Commande.h

--- Commande_Borne/Commande.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COMMANDE_H   // Directives preprocesseur pour eviter les inclusions multiples
-#define _COMMANDE_H
-
-#include <affiche.h> // Librairie permettant d'afficher sur l'ecran I2C
-#include "I2C.h"     // Librairie permettant de gerer la vitesse de transmission sur le bus I2C
-
-
-#endif
