49f6f020ff927a1f25ab965629fa8f8952c01e9b
Delaune-Remi <remidelaune.pro@gmail.com>
2017-10-05 21:01:25 +0200

    Ajout de Commentaires Modification de la ligne : char c = Wire.read()

--- Tests_Unitaire/Test_Unitaire_Clavier/Test_Unitaire_Clavier.ino
+++ Tests_Unitaire/Test_Unitaire_Clavier/Test_Unitaire_Clavier.ino
@@ -4,12 +4,13 @@
 void setup() {
   initI2C(120);
   Wire.beginTransmission(0x22); //Initialisation et Connection a au capteur (clavier matriciel) relier au Bus I2C qui a l'adresse 0x22
-  Serial.begin(9600);
+  Serial.begin(9600); // Initialisation de la liason serie relier au moniteur serie a 9600 bauds
 }
 
 void loop() {
-  char c = Wire.read();
-  Serial.write(c);
-  Serial.end();
+  char c; // Declaration d'une variable de type caractere 
+  c = Wire.read(); // Affectation de la lecture du Bus I2C a la variable c
+  Serial.print(c); // Ecriture sur le moniteur serie du caractere 
+  Serial.end(); // Arret de la liaison serie 
   Wire.endTransmission(); // Arret la transmission entre l'arduino et l'I2C
 }
