3485da7435383bb168c8f3ea67f5fbde01bafcca
Delaune-Remi <remidelaune.pro@gmail.com>
2017-10-09 12:00:51 +0200

    Ajout du programme de test pour detecter la carte a puce

---
+++ Tests_Unitaire/Test_Unitaire_Lecture_Carte/Test_Unitaire_Lecture_Carte.ino
@@ -0,0 +1,25 @@
+#include <Wire.h> /**/
+#include "I2C.h"
+
+void setup() {
+    initI2C(120);       
+    Serial.begin(9600);
+}
+
+void loop() {
+  int val;
+  
+  Wire.beginTransmission(0x21);
+  Wire.requestFrom(0x21,2); // 0x21 adresse de la carte a puce
+  val=Wire.read();
+  val&=0x01;
+  Serial.print(val);
+  Serial.end();
+  Wire.endTransmission();
+}
+
+/*
+ *        Valeurs donnees :
+ * 1 = pas de carte a puce detecte
+ * 0 = carte a puce detecte
+ */
